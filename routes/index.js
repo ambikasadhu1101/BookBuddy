b60b23f49b9df3fbb8f4c47e6c0bdfceae3c6cd8
ambika <ambikasadhu1101@gmail.com>
2020-08-09 18:12:11 +0530

    revert

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,8 @@
 const express= require('express');
-const books = require('../models/books');
 const router = express.Router();
-const Book = require('../models/books');
 
-router.get('/', async (req,res)=>{
-    let books
-    try{
-        books = await Book.find().sort({createdAt: 'desc'}).limit(10).exec();
-    }catch{
-        books = [];
-    }
-    res.render('index',{books : books});
+router.get('/', (req,res)=>{
+    res.render('index');
 })
 
 module.exports = router;
